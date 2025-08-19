4877a848c43789890d09891401abf7e51221a4c2
Alessandro coutinho <aleluinisilva@gmail.com>
2025-08-19 17:17:02 -0300

    correção no footer
    mudança do texto no footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -10,7 +10,7 @@ export const Layout = () => {
                 <Outlet/>
             </main>
             <footer className="bg-gray-800 text-white p-4 text-center fixed bottom-0 w-full">
-                <p>&copy; 2025 My Application</p>
+                <p>&copy; 2025 Central de estoque</p>
             </footer>
         </>
     )
