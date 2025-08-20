35eeafc61ba70d25cbbd78434b728fb1cb8fa340
Alessandro <aleluinisilva@gmail.com>
2025-08-20 01:19:41 -0300

    correção vite.config
    retirei o module.exports com as variaveis do tailwind pois estava dando erro

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,15 @@ export default defineConfig({
   plugins: [react(), tailwindcss(),],
 })
 
-module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        'cor-vermelha': 'var(--cor-vermelha)',
-        'cor-verde': 'var(--cor-verde)',
-      },
-    },
-  },
-}
+// module.exports = {
+//   theme: {
+//     extend: {
+//       colors: {
+//         'cor-vermelha': 'var(--cor-vermelha)',
+//         'cor-verde': 'var(--cor-verde)',
+//       },
+//     },
+//   },
+// }
+
+//aparentemente o module.exports estava dando conflito com o export default
